038e638e13e9b93d6c02c66da220a8b2e2dd527f
Tomáš Bouška <tomas@buvis.net>
2024-09-12 20:01:52 +0200

    Fix documentation in PrintZettelUseCase

--- src/doogat/core/application/use_cases/print_zettel_use_case.py
+++ src/doogat/core/application/use_cases/print_zettel_use_case.py
@@ -1,40 +1,40 @@
+"""
+Module containing the PrintZettelUseCase class.
+
+This module provides functionality for printing formatted Zettel data.
+"""
+
 from doogat.core.domain.interfaces.zettel_formatter import ZettelFormatter
 from doogat.core.domain.value_objects.zettel_data import ZettelData
 
 
 class PrintZettelUseCase:
     """
-    A use case class for printing zettel data that has been formatted according to a specified format.
+    Use case for printing formatted Zettel data.
 
-    This class is responsible for taking data encapsulated in a ZettelData object, formatting it using
-    a provided formatter that complies with the ZettelFormatter interface, and then printing the formatted output.
-
-    :param formatter: An instance of a class that implements the ZettelFormatter interface,
-                      used to format the data before printing.
-    :type formatter: ZettelFormatter
+    This class encapsulates the logic for formatting and printing Zettel data
+    using a provided ZettelFormatter.
     """
 
     def __init__(self: "PrintZettelUseCase", formatter: ZettelFormatter) -> None:
         """
-        Initializes a new instance of the PrintDoogatUseCase class.
+        Initialize the PrintZettelUseCase instance.
 
-        :param formatter: An instance of a class that implements the ZettelFormatter interface,
-                          which will be used to format the data.
-        :type formatter: ZettelFormatter
+        :param formatter: The ZettelFormatter to use for formatting Zettel data.
+        :type formatter: :class:`doogat.core.domain.interfaces.zettel_formatter.ZettelFormatter`
         """
         self.formatter = formatter
 
-    def execute(
-        self: "PrintZettelUseCase",
-        zettel_data: ZettelData,
-    ) -> None:
+    def execute(self: "PrintZettelUseCase", zettel_data: ZettelData) -> None:
         """
-        Executes the use case of printing the formatted data.
+        Execute the use case by formatting and printing the given Zettel data.
 
-        This method takes an instance of ZettelData, formats it using the formatter provided at initialization,
-        and prints the resulting string to the standard output.
+        Format the provided Zettel data using the configured formatter and print
+        the result.
 
-        :param zettel_data: The data to be formatted and printed.
-        :type zettel_data: ZettelData
+        :param zettel_data: The Zettel data to format and print.
+        :type zettel_data: :class:`doogat.core.domain.value_objects.zettel_data.ZettelData`
+        :return: None
+        :rtype: None
         """
         print(self.formatter.format(zettel_data))

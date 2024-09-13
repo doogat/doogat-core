4958a6eb1586712ed3eba29a9a2b111cd1ca0d62
Tomáš Bouška <tomas@buvis.net>
2024-09-13 16:17:55 +0200

    Document and test doogat_factory

---
+++ tests/services/test_doogat_factory.py
@@ -0,0 +1,29 @@
+from unittest.mock import MagicMock, patch
+
+import doogat.core.domain.entities as doogat_entities
+import pytest
+from buvis.pybase.formatting import StringOperator
+from doogat.core.domain.services.doogat_factory import DoogatFactory
+
+
+@pytest.fixture
+def zettel():
+    zettel = MagicMock(spec=doogat_entities.Zettel)
+    zettel.get_data = MagicMock(return_value={"some": "data"})
+    return zettel
+
+
+def test_create_with_generic_type(zettel):
+    zettel.type = ""
+    with patch.object(doogat_entities, "Zettel", return_value=zettel):
+        result = DoogatFactory.create(zettel)
+        assert result is zettel
+
+
+def test_create_with_invalid_type(zettel):
+    zettel.type = "invalid"
+    with patch.object(
+        StringOperator, "camelize", return_value="InvalidZettel"
+    ), patch.object(doogat_entities, "InvalidZettel", None, create=True):
+        result = DoogatFactory.create(zettel)
+        assert result is zettel
